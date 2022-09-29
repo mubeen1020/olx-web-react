0304c262ed22bb6df3f22e84137128c33a610107
mubeen1020 <m.mubeen1020@gmail.com>
2022-09-29 17:12:44 +0500

    olx web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ContextAllPost from "./contextStore/AllPostContext";
+import ContextAuth from "./contextStore/AuthContext";
+import ContextPost from "./contextStore/PostContext";
+import MainRoutes from "./Routes/MainRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ContextAuth>
+        <ContextAllPost>
+          <ContextPost>
+            <MainRoutes />
+          </ContextPost>
+        </ContextAllPost>
+      </ContextAuth>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+function header (){
+
+    return(
+<div className='header fixed flex '>
+<img src={require('../ui/logo.png')}/>
+</div>
+    )
+}
+export default header;
